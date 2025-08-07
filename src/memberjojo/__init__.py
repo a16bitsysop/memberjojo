9ba518d74caf0dbc9f205c6893075ef912ac05f3
Duncan Bellamy <dunk@denkimushi.com>
2025-08-07 07:11:45 +0100

    test pylint

--- src/memberjojo/__init__.py
+++ src/memberjojo/__init__.py
@@ -3,4 +3,6 @@ __doc__ = "Python module for creating and accessing databases from membermojo da
 from ._version import version as __version__
 from .mojo_member import Member, MemberData
 from .mojo_transaction import Transaction
-import noexist
+
+def my_function():
+    pass
