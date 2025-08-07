c199abdaeb3a0b949d00a7aff2e7faaebee9f984
Duncan Bellamy <dunk@denkimushi.com>
2025-08-07 07:15:32 +0100

    remove pylint test errors

--- src/memberjojo/__init__.py
+++ src/memberjojo/__init__.py
@@ -3,7 +3,3 @@ __doc__ = "Python module for creating and accessing databases from membermojo da
 from ._version import version as __version__
 from .mojo_member import Member, MemberData
 from .mojo_transaction import Transaction
-
-
-def my_function():
-    pass
