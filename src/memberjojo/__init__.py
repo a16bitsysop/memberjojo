906b9743be4a25ccb4fc365205696625918d6fdd
Duncan Bellamy <dunk@denkimushi.com>
2025-08-08 20:03:09 +0100

    use python build and use _version.py between jobs

--- src/memberjojo/__init__.py
+++ src/memberjojo/__init__.py
@@ -1,5 +1,12 @@
-__doc__ = "Python module for creating and accessing databases from membermojo data"
+"""
+memberjojo - tools for working with members.
+"""
 
-from ._version import version as __version__
+try:
+    from ._version import version as __version__
+except ModuleNotFoundError:
+    # This happens during flit build before _version.py is written
+    __version__ = "0.0.0+local"
+
 from .mojo_member import Member, MemberData
 from .mojo_transaction import Transaction
